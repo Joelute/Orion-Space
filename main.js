3bbfe376eb107dde95b7a61b1e716500d6218a00
Joseph Ho <Josephho678@gmail.com>
2023-01-21 22:13:21 -0500

    Create better fil structure

---
+++ main.js
@@ -0,0 +1,14 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+
+document.querySelector('#app').innerHTML = `
+  <img src="Background.jpg" alt="Nightscene">
+  <div class="a">
+    <h1 id="WebName">Orion Space</h1>
+
+    <a href="./home/">Orion Space</a>
+    <a href="Launch Dates.html">Launch Dates</a>
+    <a href="Training.html">Training</a>
+    <a href="Us and our motto.html">Us and our motto</a>
+  </div>
+`
