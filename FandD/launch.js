a3bb64b2574ed31ae2439420258414ad89c42765
jobshibu <122998832+jobshibu@users.noreply.github.com>
2023-02-05 00:07:28 -0600

    Added pictures to FandD, not ready to be merged

--- FandD/launch.js
+++ FandD/launch.js
@@ -1,32 +1,7 @@
-import '../style.css'
-import javascriptLogo from './javascript.svg'
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
 
-document.querySelector('#app').innerHTML = `
-    <header>
-            
-
-    <!-- Our Navigation Bar -->
-    <nav>
-        <a href="Orion_Space_Home.html">Orion Space</a>
-        <a href="Orion_Space_Launch_Dates.html">Launch Dates</a>
-        <a href="Orion_Space_Training.html">Training</a>
-        <a href="Orion_Space_About_Us.html">Us and our motto</a>
-    </nav>
-    </header>
-
-
-    <section class="contact">
-    <div class="form">
-
-    <h1>
-        
-    </h1>
-    </div>
-
-    <footer>
-    <a href="">Instagram</a>
-    <a href="">Email</a>
-    </footer>
-
-    </section>
-`
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
