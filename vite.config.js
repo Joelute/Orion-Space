60b93073c4af146ff51386cc5dd1b1b1a1999298
jobshibu <122998832+jobshibu@users.noreply.github.com>
2023-02-07 00:14:00 -0600

    Website finished
    Co-authored-by: Kris330107 <Kris330107@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         FandD: resolve(__dirname, 'FandD/FandD.html'),
+        procedure: resolve(__dirname, 'procedure/procedure.html'),
+        eligibilty: resolve(__dirname, 'eligibilty/eligibility.html'),
+        about: resolve(__dirname, 'about/about_us.html'),
+        references: resolve(__dirname, 'references/references.html'),
+
       },
     },
   },
